a09fbc15a59e44062e735ab5158115f2bf50cceb
vijay sunil kapure <kapurevijay6@gmail.com>
2024-11-16 16:24:05 +0530

    3rdcommite

--- src/test/java/sampletest/HomePageaTest.java
+++ src/test/java/sampletest/HomePageaTest.java
@@ -59,6 +59,7 @@ public class HomePageaTest extends Baseclass {
 	@AfterMethod
 	public void teardown() {
 		System.out.println("karan has update the 2nd code line");
+		System.out.println("karan has update the 3nd code line");
 
 		driver.close();
 	}
